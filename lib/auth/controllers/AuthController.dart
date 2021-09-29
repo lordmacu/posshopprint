3f28d9e4f1281bdf6530325fdaa9fda8194e56aa
Cristian garcia <cristian.garcia@pegasi.io>
2021-09-29 10:26:14 -0500

    add services and login

--- lib/auth/controllers/AuthController.dart
+++ lib/auth/controllers/AuthController.dart
@@ -1,8 +1,46 @@
 
 import 'package:get/get.dart';
+import 'package:dio/dio.dart';
+import 'package:poshop/home/controllers/HomeController.dart';
+
+import 'package:poshop/service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class AuthContoller extends GetxController{
 
   var isLogin=true.obs;
+  var isLogged=false.obs;
+  var email= "".obs;
+  var password= "".obs;
+
+  @override
+  void onInit() {
+
+  }
+
+  void loginUserSystem(login,data) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setDouble("isLogged", login);
+
+    isLogged.value=login;
+  }
+
+  login() async{
+    Client _client = new Client();
+    var _endpointProvider = new AuthProvider(_client.init());
+    try{
+      var data = await _endpointProvider.login(email,password);
+       if(data["success"]){
+        loginUserSystem(true,{});
+        return true;
+      }
+    }catch(e){
+
+      loginUserSystem(false,{});
+      return false;
+
+    }
+  }
 
 }
